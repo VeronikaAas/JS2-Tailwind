caac883cb672e2b083e24873ead955433192793f
Veronika Aas <veronikaaas@live.no>
2024-11-06 22:18:08 +0100

    Checked for correct media

--- src/js/ui/templates/postsTemplate.js
+++ src/js/ui/templates/postsTemplate.js
@@ -46,6 +46,7 @@ export function postsTemplate(posts) {
             
             mediaContainer.appendChild(media);
             clickableThumbnail.appendChild(mediaContainer);
+
         }
 
         // Post body
